64b225d3755bc45e2f40c93cdbe5f79e97009a6e
Jimmy Ng <jimmy_ng@outlook.de>
2023-06-01 13:25:24 +0200

    feat: add switch color function

---
+++ web-apps-foundation/01-light-switch/script.js
@@ -0,0 +1,12 @@
+const btn = document.querySelector(".button");
+const body = document.querySelector(".body");
+
+btn.addEventListener("click", function (event) {
+  btn.classList.toggle("btn-dark-mode");
+  body.classList.toggle("dark-mode");
+  if (document.title === "Good Morning") {
+    document.title = "Good Night";
+  } else {
+    document.title = "Good Morning";
+  }
+});
