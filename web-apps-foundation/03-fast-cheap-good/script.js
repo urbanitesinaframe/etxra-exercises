01a98b484eb250bf2999080d1856a01bef96bbf3
Jimmy Ng <jimmy_ng@outlook.de>
2023-06-01 23:13:50 +0200

    feat: adds max 2 checks. if>2, last checked=false

--- web-apps-foundation/03-fast-cheap-good/script.js
+++ web-apps-foundation/03-fast-cheap-good/script.js
@@ -5,7 +5,62 @@ const good = document.getElementById("good");
 fast.addEventListener("click", function () {
   fast.checked.toggle = true;
   fast.classList.remove("lastClicked");
-  if (document.querySelectorAll("input:checked").length === 2) {
+  if (document.querySelectorAll("input:checked").length === 3) {
+    document.querySelector(".lastClicked").checked = false;
+    document.querySelector(".lastClicked").classList.remove("lastClicked");
     fast.classList.add("lastClicked");
+  } else {
+    if (document.querySelectorAll("input:checked").length === 2) {
+      fast.classList.add("lastClicked");
+    } else {
+      if (
+        document.querySelectorAll("input:checked").length === 1 &&
+        document.querySelectorAll(".lastchecked").length === 1
+      ) {
+        document.querySelector(".lastClicked").classList.remove("lastClicked");
+      }
+    }
+  }
+});
+
+cheap.addEventListener("click", function () {
+  cheap.checked.toggle = true;
+  cheap.classList.remove("lastClicked");
+  if (document.querySelectorAll("input:checked").length === 3) {
+    document.querySelector(".lastClicked").checked = false;
+    document.querySelector(".lastClicked").classList.remove("lastClicked");
+    cheap.classList.add("lastClicked");
+  } else {
+    if (document.querySelectorAll("input:checked").length === 2) {
+      cheap.classList.add("lastClicked");
+    } else {
+      if (
+        document.querySelectorAll("input:checked").length === 1 &&
+        document.querySelectorAll(".lastchecked").length === 1
+      ) {
+        document.querySelector(".lastClicked").classList.remove("lastClicked");
+      }
+    }
+  }
+});
+
+good.addEventListener("click", function () {
+  good.checked.toggle = true;
+  good.classList.remove("lastClicked");
+  if (document.querySelectorAll("input:checked").length === 3) {
+    document.querySelector(".lastClicked").checked = false;
+    document.querySelector(".lastClicked").classList.remove("lastClicked");
+    good.classList.add("lastClicked");
+  } else {
+    if (document.querySelectorAll("input:checked").length === 2) {
+      good.classList.add("lastClicked");
+    } else {
+      if (
+        document.querySelectorAll("input:checked").length === 1 &&
+        document.querySelectorAll(".lastchecked").length === 1
+      ) {
+        document.querySelector(".lastClicked").classList.remove("lastClicked");
+      }
+    }
   }
 });
