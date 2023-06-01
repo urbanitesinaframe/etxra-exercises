9ce3c2148de4586e95eef8e5a3f9eca0e9ee4302
Jimmy Ng <jimmy_ng@outlook.de>
2023-06-01 22:26:03 +0200

    feat: add toggle checked status on click

---
+++ web-apps-foundation/03-fast-cheap-good/script.js
@@ -0,0 +1,7 @@
+const fast = document.getElementById("fast");
+const cheap = document.getElementById("cheap");
+const good = document.getElementById("good");
+
+fast.addEventListener("click", function () {
+  fast.checked.toggle = true;
+});
