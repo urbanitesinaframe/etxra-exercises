be53f78463ae822abb4490b8c1dac75308df4b51
Jimmy Ng <jimmy_ng@outlook.de>
2023-06-01 13:26:13 +0200

    upate on anonymous function

--- web-apps-foundation/01-light-switch/script.js
+++ web-apps-foundation/01-light-switch/script.js
@@ -1,7 +1,7 @@
 const btn = document.querySelector(".button");
 const body = document.querySelector(".body");
 
-btn.addEventListener("click", function (event) {
+btn.addEventListener("click", function () {
   btn.classList.toggle("btn-dark-mode");
   body.classList.toggle("dark-mode");
   if (document.title === "Good Morning") {
