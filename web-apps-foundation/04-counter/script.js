ea8161a072c1ea7778877b4d9f71c3d52ae4e6bc
Jimmy Ng <jimmy_ng@outlook.de>
2023-06-05 11:29:54 +0200

    feat everything

---
+++ web-apps-foundation/04-counter/script.js
@@ -0,0 +1,43 @@
+let count = 0;
+let backgroundSizeCount = 0;
+let counter = document.getElementById("count");
+
+// der Text innerhalb von <p> wird durch die variable count definiert
+
+function increaseCounter() {
+  count++;
+  counter.innerText = count;
+
+  if (backgroundSizeCount < 100) {
+    backgroundSizeCount++;
+  } else {
+    backgroundSizeCount = 1;
+  }
+  root.style.setProperty("--count", backgroundSizeCount + "%");
+}
+
+function resetCounter() {
+  count = 0;
+  backgroundSizeCount = 0;
+  counter.innerText = count;
+  root.style.setProperty("--count", backgroundSizeCount + "%");
+  button.blur();
+}
+
+const button = document.querySelector("button");
+
+button.addEventListener("click", resetCounter);
+
+const root = document.querySelector(":root");
+
+let main = document.querySelector("main");
+main.addEventListener("click", increaseCounter);
+
+document.addEventListener("keypress", function (e) {
+  console.log(e.key);
+  if (["Enter", " "].includes(e.key)) {
+    increaseCounter();
+  }
+});
+
+document.getElementById("count").innerHTML = count;
