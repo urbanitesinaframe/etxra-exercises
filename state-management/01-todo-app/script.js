8356dbcefb628cfe7b7275284a2ccb96d8116177
Jimmy Ng <jimmy_ng@outlook.de>
2023-06-10 12:27:03 +0200

    feat: Todos generated with input and submit

--- state-management/01-todo-app/script.js
+++ state-management/01-todo-app/script.js
@@ -1,31 +1,42 @@
-const header = document.querySelector("header");
-const main = document.querySelector("main");
-let submitBtn;
+let id = +new Date(); //to create id for ToDo Object List Elements
 
+//Fuegt Header in den Body
+function addHeader() {
+  const newHeaderEl = document.createElement("header");
+  document
+    .querySelector("body")
+    .insertBefore(newHeaderEl, document.querySelector("script"));
+}
+// Fuegt main in den body
+function addMain() {
+  const newMainEl = document.createElement("main");
+  newMainEl.id = "main";
+  document
+    .querySelector("body")
+    .insertBefore(newMainEl, document.querySelector("script"));
+}
 //Fuegt Titel hinzu
 function addTitle() {
   const newTitle = document.createElement("h1");
   newTitle.textContent = "ToDo App";
-  header.appendChild(newTitle);
+  document.querySelector("header").appendChild(newTitle);
 }
-
 //Fuegt Eingabefeld hinzu
 function addInputField() {
   const inputContainer = document.createElement("div");
   inputContainer.className = "inputContainer";
-  main.appendChild(inputContainer);
   const inputField = document.createElement("input");
   inputField.type = "text";
-  inputField.id = "toDo_test";
+  inputField.id = "toDoInput";
   inputField.name = "description";
   inputField.placeholder = "What'cha gonna do?";
   inputContainer.appendChild(inputField);
   const submitButton = document.createElement("button");
-  submitButton.id = "add-new-toDo";
+  submitButton.id = "addNewToDo";
   submitButton.innerText = "Let's plan!";
+  main.appendChild(inputContainer);
   inputContainer.appendChild(submitButton);
 }
-
 //Fuegt alle erledigte "Aufgaben-loeschen"-Feld hinzu
 function deleteDone() {
   const delBtn = document.createElement("button");
@@ -34,18 +45,12 @@ function deleteDone() {
   main.appendChild(delBtn);
 }
 
-const toInput = document.getElementById("toDo");
-const id = +new Date(); //to create id for ToDo Object List Elements
-
-/*add new key and value to object
-objektname.key(z.b. id) = 'value'
-
-*/
-
-/*function addToDo() {
-  toDoList.id = id;
-  toDoList.description = toDoDescripton.value;
-}*/
+// Fuegt ToListContainer hinzu
+function addToDoListContainer() {
+  const createNewArticleEl = document.createElement("article");
+  main.appendChild(createNewArticleEl);
+  createNewArticleEl.id = "toDoListContainer";
+}
 
 // state for status and todos
 
@@ -64,18 +69,7 @@ const state = {
       checked: false,
     },
   ],
-  todo: [
-    {
-      id: "testDatum",
-      description: "JS code works if you can read this",
-      done: false,
-    },
-    {
-      id: "testDatum2",
-      description: "JS code works if you can read this",
-      done: false,
-    },
-  ],
+  todo: [],
 };
 
 //später if done = true, checkbox checken und den Eintrag durchstreichen, Filter funktioniert auf den key: done false || true
@@ -93,7 +87,7 @@ let hours = now.getHours();
   let seconds = now.getSeconds();
   return id= year+month+date+hours+minutes+seconds
   >> +new Date()
-*/
+
 let toDo = [{ toDo: "", checked: false }];
 
 const checkbox = document.querySelector("#checkbox");
@@ -110,39 +104,54 @@ checkbox.addEventListener("change", checkStatus);
 /*for (let toDoData of state.todo) {
   const newToDoListElement = document.createElement("label");
   newToDoListElement.type;
-}*/
-
-//create list elements in html//
-function render() {
-  const toDoList = document.createElement("ul");
-  toDoList.className = "toDoList";
-  main.appendChild(toDoList);
-
-  for (let toDoListEntry of state.todo) {
-    const newToDoListElement = document.createElement("section");
-    const newToDoListEntry = document.createElement("li");
-    newToDoListEntry.id = toDoListEntry.id;
-    newToDoListEntry.innerText = toDoListEntry.description;
-    newToDoListElement.appendChild(newToDoListEntry);
-    const toDoCheckbox = document.createElement("input");
-    toDoCheckbox.type = "checkbox";
-    toDoCheckbox.name = "toDoDone";
-    toDoCheckbox.id = "toDoDoneCheckbox";
-    toDoCheckbox.check = toDoListEntry.done;
-    newToDoListElement.appendChild(toDoCheckbox);
-
-    toDoList.appendChild(newToDoListElement);
-  }
-}
-/* submitBtn = document.getElementById("add-new-toDo");
-  submitBtn.addEventListener(
-    "click",
-    document.querySelector(".toDoListContainer").appendChild(toDoListe)
-  ); //hier muss eine function zum hinzufuegen des neues objektes in die objektliste rein
 }
 */
+
+//create ToDo list elements in html//
+function render() {
+  if (state.todo.length === 0) {
+    toDoListContainer.innerHTML = "Type in your 1st ToDo to create a list";
+  } else {
+    const createToDoList = document.createElement("ul");
+    createToDoList.id = "toDoList";
+
+    for (let toDoListEntry of state.todo) {
+      const newToDoListElement = document.createElement("section");
+      const newToDoListEntry = document.createElement("li");
+      newToDoListEntry.id = toDoListEntry.id;
+      newToDoListEntry.innerText = toDoListEntry.description;
+      newToDoListElement.appendChild(newToDoListEntry);
+      const toDoCheckbox = document.createElement("input");
+      toDoCheckbox.type = "checkbox";
+      toDoCheckbox.name = "toDoDone";
+      toDoCheckbox.id = "toDoDoneCheckbox";
+      toDoCheckbox.check = toDoListEntry.done;
+      newToDoListElement.appendChild(toDoCheckbox);
+      createToDoList.appendChild(newToDoListElement);
+    }
+    toDoListContainer.appendChild(createToDoList);
+  }
+}
+
+addHeader();
+addMain();
 addTitle();
+
 deleteDone();
 addInputField();
-
+addToDoListContainer();
 render();
+
+function addNewToDo() {
+  state.todo.push({
+    id: +new Date(),
+    description: toDoInput.value,
+    done: false,
+  });
+  toDoListContainer.innerHTML = "";
+  toDoInput.value = "";
+  render();
+}
+
+const submitBtn = document.querySelector("#addNewToDo");
+submitBtn.addEventListener("click", addNewToDo); //hier muss eine function zum hinzufuegen des neues objektes in die objektliste rein
